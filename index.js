0724051e886be63abed8807f99e241bec49f2f9d
Woodney Juste <woodneydev@gmail.com>
2023-08-13 20:38:39 -0400

    qaulity of life

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use("/videos", videos);
 
-app.listen(PORT, () => {
+app.listen(PORT || 8080, () => {
     console.log("Server is running on port " + PORT);
     console.log("Type CTRL + C to exit");
 });
